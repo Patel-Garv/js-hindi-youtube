b825ad3e0ffc6bbfaa283e6ee8f541ce51822ae7
Patel-Garv <patelgarv401@gmail.com>
2024-08-06 16:21:30 +0000

    Variable done

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,12 @@
+const accountId= 123456
+let accountemail= "xyz@google.com"
+var accountpassword= "23456"
+accountcity = "Ahemdabad"
+let accountstate= "Gujarat"
+
+accountemail= "abc@google.com"
+accountpassword= "4567889"
+accountcity= "Surat"
+
+console.table([accountemail,accountpassword,accountcity,accountstate]);
+
