8f3275c434280753eb3604fc2a3e06188679e7c1
Patel-Garv <patelgarv401@gmail.com>
2024-08-07 12:41:11 +0000

    datatypes

---
+++ 01_basic/02_datatypes.js
@@ -0,0 +1,36 @@
+"Use strict"; //treat all Js code as newer version
+
+//alert(3+3) //we are using nodjs, not console browser
+
+console.log(3
+     +3) // code redability should be high
+
+
+console.log("Hitesh")
+
+
+let Name = "Garv"
+let age = 20
+let isLoggedIn = false
+let state;
+
+console.table([Name,age,isLoggedIn]);
+
+
+
+// ******** Datatypes *******
+// number => 2 to power 53
+// bigint
+//string => ""
+//boolean => true/false
+//null => standalone value
+//undefined =>
+// symbol => unique
+
+
+
+//object 
+
+
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined
