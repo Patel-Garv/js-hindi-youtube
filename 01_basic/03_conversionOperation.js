9d30ac2b50d565ffdfc8d55387ffd12f7c36d078
Patel-Garv <patelgarv401@gmail.com>
2024-08-08 04:26:52 +0000

    conversion of datatypes

---
+++ 01_basic/03_conversionOperation.js
@@ -0,0 +1,33 @@
+let score = "Garv"
+
+//console.log(typeof score);
+//console.log(typeof (score));
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+
+// "33" => 33
+// "33abc" => NaN (Not a Number)
+//  true => 1;   false => 0;
+
+
+let isLoggedIn = "Garv"
+
+let booleanisLoggedIn = Boolean(isLoggedIn)
+console.log(typeof booleanisLoggedIn)
+console.log(booleanisLoggedIn);
+
+// 1 => true;  0 => false
+// "" => false
+// "Garv" => true
+
+
+
+let someNumber = 33
+
+let stringsomeNumber = String(someNumber)
+console.log(stringsomeNumber)
+console.log(typeof stringsomeNumber);
+
