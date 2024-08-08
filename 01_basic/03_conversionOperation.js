fef7a1dd56023ee1dabdb950a2b98b592eb1ba43
Patel-Garv <patelgarv401@gmail.com>
2024-08-08 05:42:54 +0000

    conversion of Operation

--- 01_basic/03_conversionOperation.js
+++ 01_basic/03_conversionOperation.js
@@ -16,8 +16,8 @@ let valueInNumber = Number(score)
 let isLoggedIn = "Garv"
 
 let booleanisLoggedIn = Boolean(isLoggedIn)
-console.log(typeof booleanisLoggedIn)
-console.log(booleanisLoggedIn);
+//console.log(typeof booleanisLoggedIn)
+//console.log(booleanisLoggedIn);
 
 // 1 => true;  0 => false
 // "" => false
@@ -28,6 +28,62 @@ console.log(booleanisLoggedIn);
 let someNumber = 33
 
 let stringsomeNumber = String(someNumber)
-console.log(stringsomeNumber)
-console.log(typeof stringsomeNumber);
+//console.log(stringsomeNumber)
+//console.log(typeof stringsomeNumber);
+
+
+
+// ****************** Operations ********************
+
+let value = 3
+let negvalue = -value
+//console.log(negvalue);
+
+
+//console.log(2+2);
+//console.log(2-2);
+//console.log(2*2);
+//console.log(2**3);
+//console.log(2/2);
+//console.log(2 % 2);
+
+
+let str1 = "Garv"
+let str2 = " Patel"
+
+let str3 = str1 + str2
+//console.log(str3);
+
+//console.log("1" + 2);
+//console.log(1 + "2");
+//console.log("1" + 2 + 2);
+//console.log(1 + 2 + "2");
+
+//console.log( (3 +4) * 5 % 3);
+
+
+//console.log(+true);
+//console.log(+ "");
+
+
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2+2
+
+
+let gameCounter = 200
+gameCounter ++;
+console.log(gameCounter);
+
+
+// Link to study
+//https://262.ecma-international.org/14.0/index.html?_gl=1*1xvaiqg*_ga*MjQ1Nzk4NDI2LjE3MjMwMTAyMTQ.*_ga_TDCK4DWEPP*MTcyMzA5NDU5OS41LjEuMTcyMzA5NTQzNy4wLjAuMA..
+
+
+
+
+
+
+
 
