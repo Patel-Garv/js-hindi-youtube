a5bd8142e969c0cd2386e128365b0e6ac566039b
Patel-Garv <patelgarv401@gmail.com>
2024-10-02 05:29:17 +0000

    variable2

--- 01_basic/01_variables.js
+++ 01_basic/01_variables.js
@@ -4,9 +4,15 @@ var accountpassword= "23456"
 accountcity = "Ahemdabad"
 let accountstate= "Gujarat"
 
+//accountId = 2 // not allowed
+
 accountemail= "abc@google.com"
 accountpassword= "4567889"
 accountcity= "Surat"
 
+/*
+prefer not to use var
+because of issue in block scope and functional scope 
+*/
 console.table([accountemail,accountpassword,accountcity,accountstate]);
 
